2ad88a91a80e4f6581b9efd653deb1dec5d24a07
Spenser Kao <SpenserKao@Optusnet.com.au>
2018-05-10 00:56:40 +1000

    First check-in.

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# Employee-Data-Navigation
+# Employee Data Navigation
 An application to navigate hypothetical employee data.
+
+## Implemented features
+Following four views have been implemented:
+   - Full employees list
+   - Hierarchical relationship among employees
+   - Employee without manager
+   - Manager who is not a valid employee
+ 
+## Execution
+### via Plunkr
+The application is hosted under Plnkr [1]. Simply open that URL for execution.
+ 
+### Locally
+Just in case, you need to execute it with web server, Firefox and Microsoft Internet Explorer (Microsoft Edge) work for the application perfectly. 
+Simply open index.html file with internet browser.
+Google Chrome is not supported, due to following error "Cross origin requests are only supported for protocol schemes: http, data, chrome, chrome-extension, https." 
+
+## Code Review
+Two ways of reviewing code: GitHub [2].
+
+## Technologies in use
+### AngularJS
+Angular JS ver 1.1.5 has been utilised in this application, noteworthily following directives have been employed:
+   - ng-app - designates the root element of the application and is typically placed near the root element of the page - e.g. on the <body> or <html> tags
+   - ng-controller - attaches a controller class to the view
+   - ng-show - enables conditional display of selected image's details 
+   - ng-repeat - implements images thumbnail
+   - ng-model - binds view into model required by an input that specifies keywords (tags) for search
+   - ng-switch & ng-switch-when - conditionally swaps DOM structure on your template based on a scope expression
+   - ng-selected - sets the selected attribute on the element
+   - ng-change - evaluates the given expression when the user changes the input
+
+## Design of employee data input
+Simplistic JSON-based flat files have been facilitated as inputs for employee data.
+
+## Test cases
+Two JSON-based flat files have been built to covern differenct scenarios.
+   - employeeData.json - contains indentical content as examplied in the Momenton-provided requirement document
+   - employeeData-depth5.json - currently used as a test case, a superset of above file, but with following extract senarios:
+      - Manager who is not a valid employee - this isn't covered in the original requirement document
+	  - Multiple topmost managers, rather than just one CEO, can also be handled
+	  - Hierarchy level has been extended to five
+
+## ToDo
+| item | Description | Implemented? (Y/N) |
+| ---:|:-------------|:-----:|
+|1| To generically determine number of td (HTML term: table cell) elemnts in order to allow unlimted hierarchy levels. Currently we fixed it as five | N |
+
+## Reference
+[1] https://run.plnkr.co/plunks/QpxnTeHwtiWfIAI9o1KZ/ <br/>
+[2] https://embed.plnkr.co/QpxnTeHwtiWfIAI9o1KZ/ <br/>
---
+++ script.js
@@ -0,0 +1,169 @@
+var empDataApp = angular.module('empDataApp', []);
+empDataApp.controller('browseEmpData', ['$scope', '$http', 'showHTMLcontent', function($scope, $http, showHTMLcontent) {
+	$http.get('employeeData-depth5.json').success(function(data) {
+		$scope.renderOption = "opt1";
+		$scope.searchResult = $scope.empData = data.employeeData;
+		$scope.currentLevel = 0;
+		$scope.maxDepth = 0;
+		$scope.hierarchy = [];
+	});
+
+	$http.get('renderOptions.json').success(function(data) {
+		$scope.renderOptions = data.renderOptions;
+	});
+	
+	$scope.changeFunc = function (opt) {
+		$scope.renderOption = opt;
+
+		switch($scope.renderOption) {
+			case 'opt1':
+				$scope.searchResult = $scope.empData;
+				break;
+			case 'opt2':
+				// Hierarchical relationship among employees
+				$scope.currentLevel = 0;
+				$scope.hierarchy = [];				
+				$scope.formHierarchy("");
+				//alert("$scope.hierarchy.length=" + $scope.hierarchy.length + " and content=" + JSON.stringify($scope.hierarchy));
+				break;
+			case 'opt3':
+				// Employee without manager, for e.g., the CEO.
+				$scope.searchResult = $scope.empData.filter(function (n, i){
+					return n.managerId === "";
+				});
+				break;
+			case 'opt4':
+				// Manager who is not a valid employee
+				var uniqueMgrIDs = $scope.findUniqueManagerIDs("managerId");
+				var uniqueIDs = $scope.findUniqueManagerIDs("id");	
+				var invalidEmployeeIDs = $scope.findInvalidEmployeeIDs(uniqueMgrIDs, uniqueIDs);
+				$scope.searchResult = $scope.findEmployeesWhoseMamangerIdInvalid(invalidEmployeeIDs);				
+				break;				
+		}		
+	};
+
+	$scope.findUniqueManagerIDs = function (type) {	
+		var lookup = {};
+		var items = $scope.empData;
+		var result = [];
+
+		items.forEach(function(item) {
+			var name = (type === "managerId" ? item.managerId : item.id);
+
+			if (!(name in lookup)) {
+				lookup[name] = 1;
+				result.push(name);
+			}
+		});		
+		return result;
+	};
+
+	$scope.findInvalidEmployeeIDs = function (uniqueManagerIDs, uniqueIDs) {	
+		var invalidEmployeeIDs = [];
+		
+		uniqueManagerIDs.forEach(function(mgrID) {
+			if (!($scope.checkAvailability(uniqueIDs, mgrID))) {
+				invalidEmployeeIDs.push(mgrID);  
+			}	
+		});			
+		return invalidEmployeeIDs;
+	};	
+	
+	$scope.checkAvailability = function (arr, val) {
+		return arr.some(function(arrVal) {
+			return val === arrVal;
+		});
+	};
+	
+	$scope.findEmployeesWhoseMamangerIdInvalid = function (arr) {
+		var items = $scope.empData;
+		var result = [];
+
+		arr.forEach(function(mgrID) {	
+			items.forEach(function(item) {
+				if (mgrID !== "" && mgrID === item.managerId) {
+					result.push(item);
+				}
+			});				
+		});				
+		return result;
+	};
+
+	$scope.formHierarchy = function (id) {
+		/** 
+		 *	Find the depth of the employees data structure
+		 */
+		var items = $scope.empData; 
+		var mgrs = {}; 
+		var immediateSubordinates = {}; 
+
+		// 1. find immediate subordinates of the employee, if exist
+		mgrs = $scope.empData.filter(function (n, i){
+			return n.managerId === id;
+		});
+		$scope.currentLevel += (mgrs != null ? 1 : 0);		
+		$scope.maxDepth = Math.max($scope.currentLevel, $scope.maxDepth);
+				
+		// 2. traverse from EACH of the immediate subordinates, until reaching his/her 
+		// lowest level (leaf) staff, whose id is not a managerId. Once done, record the depth.
+		// Whenever a depth is recorded, compare it with previous one, only keep the 
+		// bigger one. Eventually the depth should be the biggest one, which we are after		
+		mgrs.forEach(function(subr) {
+			var oneLine = {
+				"name" : subr.employeeName,
+				"level" : $scope.currentLevel
+			};	
+			$scope.hierarchy.push(oneLine);
+			
+			if ($scope.isManager(subr.id)) {
+				// start recursion here!
+				$scope.formHierarchy(subr.id);
+			} else {
+				// an employee without subordinate
+			}
+		});
+		
+		$scope.currentLevel -= 1;
+	};
+
+	$scope.isManager = function (id) {
+		return $scope.empData.some(function(oneEmployee) {
+			return id === oneEmployee.managerId;
+		});
+	};
+
+	
+	/*
+	$scope.formTree = function () {	
+		var data = $scope.empData;
+		var root = "";
+		
+		return data.reduce((t,o) => {
+			o.id === o.managerId && (root = o.id);
+			t[o.id]  ? t[o.id].employeeName = o.employeeName
+					: t[o.id] = {id: o.id, name: o.employeeName};
+			t[o.managerId] ? o.managerId !== o.id ? t[o.managerId].children.push(t[o.id])
+									 : t[o.managerId].children = t[o.managerId].children || []
+					: t[o.managerId] = {id: o.managerId, children: [t[o.id]]};
+			return t;
+		},{})[root];
+	};
+	*/
+}]);
+
+/**
+ * Following configuration is to force sto remove unwanted "#/index.html" suffix from URL when refreshing.
+ * ref: http://stackoverflow.com/questions/14771091/removing-the-fragment-identifier-from-angularjs-urls-symbol
+ *		https://docs.angularjs.org/api/ng/provider/$locationProvider
+ */
+empDataApp.config(['$locationProvider', function($locationProvider) {
+	$locationProvider.html5Mode(true);
+}]);
+
+empDataApp.factory('showHTMLcontent', function() {
+	return {
+		htmlOut: function(url) {
+			return url;
+		}
+	}
+});
